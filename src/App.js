3dfb1dfe2637a58912ed59f140df827507fa8edd
yusufziyayildirim <yusuffziyayildirim@gmail.com>
2022-08-03 21:03:44 +0300

    TotalCompleteItems component activated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './style.css';
 import AddTodoForm from './components/AddTodoForm';
 import TodoList from './components/TodoList';
+import TotalCompleteItems from './components/TotalCompleteItems';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="bg-[#10101d] min-h-min w-[278px] sm:w-[520px] md:w-[620px] rounded-xl px-6 py-8">
         <AddTodoForm />
         <TodoList />
+        <TotalCompleteItems />
       </div>
     </div>
   );
--- src/components/TotalCompleteItems.js
+++ src/components/TotalCompleteItems.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const TotalCompleteItems = () => {
-  return (
-    <div>TotalCompleteItems</div>
-  )
+
+    const completedTodos = useSelector((state) =>
+        state.todos.filter((todo) => todo.completed === true)
+    )
+
+    return (
+        <h4 className='mt-5 text-white text-center font-semibold text-lg'>
+            Total Complete Items: {completedTodos.length}
+        </h4>
+    )
 }
 
 export default TotalCompleteItems
