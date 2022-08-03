f006fc67716033a09bc63bc368088eed1888068a
yusufziyayildirim <yusuffziyayildirim@gmail.com>
2022-08-03 14:55:25 +0300

    TodoItem component designed

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,8 +1,23 @@
+import { BsCheck, BsTrash } from 'react-icons/bs'
+import { FiEdit } from 'react-icons/fi'
 
 const TodoItem = () => {
-  return (
-    <div>TodoItem</div>
-  )
+    return (
+        <div className="flex items-center justify-between w-full bg-[#191933] p-3 rounded mt-2">
+            <div className='flex items-center gap-3'>
+                <div className="w-5 h-5 rounded-full border border-white hover:border-[#ee9ca7] hover:btn-shadow flex items-center justify-center cursor-pointer">
+                    <BsCheck className='text-[#ffffff80]' />
+                </div>
+                <span className='text-white font-semibold text-base cursor-pointer'>
+                    TodoName
+                </span>
+            </div>
+            <div className='flex items-center gap-1'>
+                <FiEdit size={20} className="cursor-pointer text-blue-600 hover:scale-95 transition-all" />
+                <BsTrash size={20} className="cursor-pointer text-red-600 hover:scale-95 transition-all" />
+            </div>
+        </div >
+    )
 }
 
 export default TodoItem
