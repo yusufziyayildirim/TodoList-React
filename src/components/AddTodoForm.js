740fe14ef52c079eea598da1a457e168cc355f75
yusufziyayildirim <yusuffziyayildirim@gmail.com>
2022-08-03 14:52:09 +0300

    AddTodoForm component designed

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,7 +1,19 @@
+import { MdAdd } from 'react-icons/md'
 
 const AddTodoForm = () => {
   return (
-    <div>AddTodoForm</div>
+    <div>
+        <h3 className="text-white font-semibold text-2xl mb-6">TO DO LIST</h3>
+            <form className="flex items-center justify-between gap-2">
+                <input
+                    type="text"
+                    className='outline-none text-white h-8 w-full bg-transparent border-b'
+                    name="add_task"
+                    placeholder="Add New Task"
+                />
+                <MdAdd className="text-white bg-[#ee9ca7] rounded-full cursor-pointer btn-shadow hover:scale-90 transition-all" size={35} />
+            </form>
+    </div>
   )
 }
 
