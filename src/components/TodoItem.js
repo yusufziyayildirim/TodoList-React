0f03e7a6fbcbb24a3d49a5cf08e89467113cf627
yusufziyayildirim <yusuffziyayildirim@gmail.com>
2022-08-03 15:21:43 +0300

    Todos listed

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,17 @@
 import { BsCheck, BsTrash } from 'react-icons/bs'
 import { FiEdit } from 'react-icons/fi'
 
-const TodoItem = () => {
+const TodoItem = ({ todo }) => {
     return (
         <div className="flex items-center justify-between w-full bg-[#191933] p-3 rounded mt-2">
             <div className='flex items-center gap-3'>
                 <div className="w-5 h-5 rounded-full border border-white hover:border-[#ee9ca7] hover:btn-shadow flex items-center justify-center cursor-pointer">
-                    <BsCheck className='text-[#ffffff80]' />
+                    {todo.completed &&
+                        <BsCheck className='text-[#ffffff80]' />
+                    }
                 </div>
-                <span className='text-white font-semibold text-base cursor-pointer'>
-                    TodoName
+                <span className={`text-white font-semibold text-base cursor-pointer ${todo.completed ? 'line-through' : ''}`}>
+                    {todo.title}
                 </span>
             </div>
             <div className='flex items-center gap-1'>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,17 @@
 import TodoItem from './TodoItem';
+import { useSelector } from 'react-redux';
 
 const TodoList = () => {
+    const todos = useSelector((state) => state.todos)
+
     return (
         <div className="mt-4">
-            <TodoItem />
+            {console.log(todos)}
+              {todos &&
+                    todos.map(todo => (
+                        <TodoItem key={todo.id} todo={todo} />
+                    ))
+                }
         </div>
     )
 }
