6740fb2c536dea8303f0a9761abc50821e7a330f
yusufziyayildirim <yusuffziyayildirim@gmail.com>
2022-08-03 14:49:22 +0300

    Background designed

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="h-[100vh] flex flex-col items-center justify-center">
-      <AddTodoForm />
-      <TodoList />
+    <div className="h-[100vh] flex flex-col items-center justify-center bg-app">
+      <div className="bg-[#10101d] min-h-min w-[620px] rounded-xl px-6 py-8">
+        <AddTodoForm />
+        <TodoList />
+      </div>
     </div>
   );
 }
